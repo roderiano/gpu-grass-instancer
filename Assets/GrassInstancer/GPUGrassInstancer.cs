787c4d675b2268233d20a07b4ed2b29909d9cb91
unknown <gabrielsilveirapro@gmail.com>
2022-12-06 21:39:38 -0300

    first commit

---
+++ Assets/GrassInstancer/GPUGrassInstancer.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Random = System.Random;
+using System.Collections.Generic;
+
+
+
+public class GPUGrassInstancer : MonoBehaviour {
+    public Material material;
+    public Mesh mesh;
+    public MeshFilter meshFilter;
+
+    GraphicsBuffer commandBuf;
+    GraphicsBuffer.IndirectDrawIndexedArgs[] commandData;
+    const int commandCount = 11;
+
+    void Start()
+    {
+        commandBuf = new GraphicsBuffer(GraphicsBuffer.Target.IndirectArguments, commandCount, GraphicsBuffer.IndirectDrawIndexedArgs.size);
+        commandData = new GraphicsBuffer.IndirectDrawIndexedArgs[commandCount];
+    }
+
+    void OnDestroy()
+    {
+        commandBuf?.Release();
+        commandBuf = null;
+    }
+
+    void Update()
+    {
+        RenderParams renderParams = new RenderParams(material);
+        renderParams.worldBounds = new Bounds(Vector3.zero, 10000*Vector3.one); // use tighter bounds for better FOV culling
+        renderParams.matProps = new MaterialPropertyBlock();
+       
+
+        List<Vector3> terrainVert = new List<Vector3>();
+        meshFilter.mesh.GetVertices(terrainVert);
+
+        foreach (Vector3 vert in terrainVert.ToArray())
+        {
+            Vector3 newPos = vert + transform.position;
+            renderParams.matProps.SetMatrix("_ObjectToWorld", Matrix4x4.TRS(newPos, transform.rotation, Vector3.one));
+            for (int i = 0; i < commandCount; i++)
+            {
+                commandData[i].indexCountPerInstance = mesh.GetIndexCount(0);
+                commandData[i].instanceCount = commandCount;
+            }
+        }
+
+        commandBuf.SetData(commandData);
+        Graphics.RenderMeshIndirect(renderParams, mesh, commandBuf, commandCount);
+    }
+}
